ecdc7805ee74a7f23566efe236675f398e8ba643
robertlongmendez <82781708+robertlongmendez@users.noreply.github.com>
2022-03-07 19:05:27 -0600

    Exercise 1
    in class exercise

---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Rails.application.routes.draw do
+  get '/myprofile', to: 'pages#profile'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
